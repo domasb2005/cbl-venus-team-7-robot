ca318268be6d59673b60df3e5051c15f905f20e5
domasb2005 <domas.berulis@gmail.com>
2025-05-09 07:55:25 +0000

    motors joystick

--- applications/motors/main.c
+++ applications/motors/main.c
@@ -2,43 +2,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stepper.h>
+#include <termios.h>
+#include <unistd.h>
 
-#define MIN_SPEED 60000
-#define MAX_SPEED 10000
-#define NUM_INCREMENTS 20
+// Function to configure terminal for immediate key reading
+void configure_terminal(struct termios *old) {
+    struct termios new;
+    tcgetattr(STDIN_FILENO, old);
+    new = *old;
+    new.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &new);
+}
 
-int sweep_distance = 2000; // Total number of steps to move during accel/decel
-
-void sweep(int distance) {
-    int speed;
-    int step_chunk = distance / NUM_INCREMENTS;
-
-    // Accelerate: MIN_SPEED -> MAX_SPEED
-    for (int i = 0; i < NUM_INCREMENTS; i++) {
-        speed = MIN_SPEED - ((MIN_SPEED - MAX_SPEED) * i) / NUM_INCREMENTS;
-        stepper_set_speed(speed, speed);
-        stepper_steps(step_chunk, step_chunk);
-    }
-
-    // Decelerate: MAX_SPEED -> MIN_SPEED
-    for (int i = 0; i < NUM_INCREMENTS; i++) {
-        speed = MAX_SPEED + ((MIN_SPEED - MAX_SPEED) * i) / NUM_INCREMENTS;
-        stepper_set_speed(speed, speed);
-        stepper_steps(step_chunk, step_chunk);
-    }
+// Function to restore terminal settings
+void restore_terminal(struct termios *old) {
+    tcsetattr(STDIN_FILENO, TCSANOW, old);
 }
 
 int main(void) {
+    struct termios old_settings;
+    char key;
+    int left_speed, right_speed;
+    const int STEP_COUNT = 200;  // Variable for step count
+    const int CUSTOM_SPEED = 15000;  // Our custom speed
+    
+    // Calculate microseconds per step at our custom speed
+    // If 3072 steps/s = 30us, then 10000 steps/s = (30 * 3072) / 10000 us
+    const float US_PER_STEP = (30.0f * 3072) / CUSTOM_SPEED;  // ≈ 9.216 us per step
+    
+    // Calculate total time for steps in milliseconds
+    // Time = (steps * microseconds per step) / 1000 to get milliseconds
+    int STEP_TIME_MS = (int)((STEP_COUNT * US_PER_STEP) / 1000);
+    // STEP_TIME_MS -= 100;
+    
     pynq_init();
     stepper_init();
     stepper_enable();
-
+    
+    // Configure terminal for immediate key reading
+    configure_terminal(&old_settings);
+    
+    printf("Use arrow keys to control. Press 'q' to quit.\n");
+    printf("Each movement will take approximately %d milliseconds\n", STEP_TIME_MS);
+    
     while (1) {
-        sweep(sweep_distance);
-        // Optional: reverse direction
-        sweep(-sweep_distance);
+        if (read(STDIN_FILENO, &key, 1) == 1) {
+            left_speed = CUSTOM_SPEED;   // Fixed: Using CUSTOM_SPEED instead of MAX_SPEED
+            right_speed = CUSTOM_SPEED;
+            
+            if (key == '\033') { // Escape sequence
+                read(STDIN_FILENO, &key, 1); // Skip '['
+                read(STDIN_FILENO, &key, 1); // Get actual key
+                
+                switch (key) {
+                    case 'A': // Up arrow
+                        stepper_set_speed(left_speed, right_speed);
+                        stepper_steps(STEP_COUNT, STEP_COUNT);
+                        // sleep_msec(STEP_TIME_MS);
+                        break;
+                    case 'B': // Down arrow
+                        stepper_set_speed(left_speed, right_speed);
+                        stepper_steps(-STEP_COUNT, -STEP_COUNT);
+                        // sleep_msec(STEP_TIME_MS);
+                        break;
+                    case 'C': // Right arrow
+                        stepper_set_speed(left_speed, right_speed);
+                        stepper_steps(-STEP_COUNT, STEP_COUNT);
+                        // sleep_msec(STEP_TIME_MS);
+                        break;
+                    case 'D': // Left arrow
+                        stepper_set_speed(left_speed, right_speed);
+                        stepper_steps(STEP_COUNT, -STEP_COUNT);
+                        // sleep_msec(STEP_TIME_MS);
+                        break;
+                }
+            } else if (key == 'q') {
+                break;
+            }
+        }
     }
-
+    
+    // Cleanup
+    restore_terminal(&old_settings);
     stepper_disable();
     stepper_destroy();
     pynq_destroy();
